4fa1177a037d501d4db82311a464a1dcf8054e7a
EMD <enrique.darderes@gmail.com>
2019-10-19 14:32:17 -0300

    error handling

--- server.js
+++ server.js
@@ -30,39 +30,47 @@ app.post("/test", async function (req, res){
 
 app.post("/get_products", async function(req, res){
 
-    let product = req.body.product;
-    console.log("RUNNING")
+    try{
+            
+        let product = req.body.product;
+        console.log("RUNNING")
 
-    let dia = await api_handler.get_product.dia(product);
-    await dia;
-    console.log(dia);
-    let coto = await api_handler.get_product.coto(product);
-    await coto
-    console.log(coto)
-    let disco = await api_handler.get_product.disco(product);
-    await disco
-    console.log(disco)
+        let dia = await api_handler.get_product.dia(product);
+        await dia;
+        console.log(dia);
+        let coto = await api_handler.get_product.coto(product);
+        await coto
+        console.log(coto)
+        let disco = await api_handler.get_product.disco(product);
+        await disco
+        console.log(disco)
 
 
-    let send_object = [];
+        let send_object = [];
 
-    for(let i = 0; i < dia.length; i++){
-        send_object.push(dia[i]);
+        for(let i = 0; i < dia.length; i++){
+            send_object.push(dia[i]);
+        }
+
+        for(let o = 0; o < coto.length; o++){
+            send_object.push(coto[o]);
+        }
+
+        for(let u = 0; u < disco.length; u++){
+            send_object.push(disco[u]);
+        }
+
+    
+
+        console.log(send_object);
+
+        res.status(200).send(send_object).end();
+        
+    }catch(err){
+        console.log("Error", err);
+        res.status(404).end();
     }
 
-    for(let o = 0; o < coto.length; o++){
-        send_object.push(coto[o]);
-    }
-
-    for(let u = 0; u < disco.length; u++){
-        send_object.push(disco[u]);
-    }
-
-  
-
-    console.log(send_object);
-
-    res.status(200).send(send_object).end();
 })
 
 app.get("/", function(req, res){
